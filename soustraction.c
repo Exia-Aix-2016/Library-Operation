eb8bc2f3e0462840c42813715541df8b0acb96c0
Unknown <kinghyde111@gmail.com>
2016-11-04 11:21:44 +0100

    soustraction

---
+++ soustraction.c
@@ -0,0 +1,23 @@
+//
+// Created by Baptiste Fischini on 04/11/2016.
+//
+
+#include "soustraction.h"
+
+void soustraction ()
+{
+    int a;
+    int b;
+    int c;
+
+    printf("Entrez a : ");
+    scanf("%d", &a);
+    printf("Entrez b : ");
+    scanf("%d", &b);
+
+    c = a - b;
+
+    printf("Le resultat est : %d", c);
+}
+
+
