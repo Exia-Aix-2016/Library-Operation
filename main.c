3bca5aa4d15d2bee3f2931e11666f4c5bd8e91c2
Hougo13 <hougo13@gmail.com>
2016-11-04 10:11:24 +0100

    Addition

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "addition.h"
 
 int main()
 {
     printf("Hello world!\n");
     printf("test");
+
+    printf("%d\n", addition(2,3));
 
     return 0;
 }
